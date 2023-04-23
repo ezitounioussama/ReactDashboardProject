9ffd3c5e86b7a9f6cec116a6ed7aa683ebbc239d
ezitounioussama <zitounioussama95@gmail.com>
2023-04-24 00:14:14 +0100

    SparkLine Error!

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+import { render } from "react-dom";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, color, width, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
