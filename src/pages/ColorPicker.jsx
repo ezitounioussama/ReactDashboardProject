015bc1753a8857e447e59b7d1427cbe56bba5591
ezitounioussama <zitounioussama95@gmail.com>
2023-04-25 21:37:03 +0100

    color picker is good for now !

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
+import { Header } from "../components";
+
+const change = (Args) => {
+  document.getElementById("preview").style.backgroundColor =
+    Args.currentValue.hex;
+};
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Color Picker" />
+      <div className="text-center">
+        <div id="preview" />
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <div>
+            <p className="text-2xl mt-2 mb-4 font-semibold">Inline Palette</p>
+            <ColorPickerComponent
+              id="inline-palette"
+              mode="Palette"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+          </div>
+          <div>
+            <p className="text-2xl mt-2 mb-4 font-semibold">Inline Picker</p>
+            <ColorPickerComponent
+              id="inline-palette"
+              mode="Picker"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ColorPicker;
