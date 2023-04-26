09e15e2cfea7d75f256975ed7dd496dc5d6416a8
ezitounioussama <zitounioussama95@gmail.com>
2023-04-26 20:10:40 +0100

    all components was succesfully working !!

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { ChartsHeader, LineChart } from "../../components";
+import { Header } from "../../components";
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export default Line;
