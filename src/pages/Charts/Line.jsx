0fb639e62ff11f800a9f4ba295241cdd7f12f92a
ezitounioussama <zitounioussama95@gmail.com>
2023-04-26 20:27:34 +0100

    charts was succesfully working with syncfusion

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+
 import { ChartsHeader, LineChart } from "../../components";
-import { Header } from "../../components";
-const Line = () => {
-  return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="Chart" title="Inflation Rate" />
-      <div className="w-full">
-        <LineChart />
-      </div>
+
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Line" title="Inflation Rate" />
+    <div className="w-full">
+      <LineChart />
     </div>
-  );
-};
+  </div>
+);
 
 export default Line;
